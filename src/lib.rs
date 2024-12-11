20c2009bb9ac2635d2a94266dd77895c61a10740
parrrate <tim@ongoteam.yaconnect.com>
2024-12-11 22:02:54 +0000

    doctest

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,33 @@
 //! Generic implementation of a Feistel Cipher.
+//!
+//! ```rust
+//! # #[cfg(feature = "array")]
+//! # fn main() {
+//! # use sha2::{Digest, Sha256};
+//! # use generic_array::ConstArrayLength;
+//! # use feistel::*;
+//! # let key: [u8; 32] = std::array::from_fn(|i| i as _);
+//! let network = key
+//!     .chunks_exact(4)
+//!     .map(|chunk| {
+//!         move |half: &XorArray<u8, ConstArrayLength<32>>| {
+//!             let mut hasher = Sha256::new();
+//!             hasher.update(half);
+//!             hasher.update(chunk);
+//!             let value: [u8; 32] = hasher.finalize().into();
+//!             XorArray(value.into())
+//!         }
+//!     })
+//!     .feistel_symmetric();
+//! let original = [0; 64].into();
+//! let encrypted = network.clone().array_encrypt(original);
+//! assert_ne!(original, encrypted);
+//! let decrypted = network.clone().array_decrypt(encrypted);
+//! assert_eq!(original, decrypted);
+//! # }
+//! # #[cfg(not(feature = "array"))]
+//! # fn main() {}
+//! ```
 
 use std::ops::BitXor;
 
